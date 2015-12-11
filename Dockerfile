2be7fb6fde03290885a27d56a03e454ed9e98135
Markus H <markus.heider@bindoc.de>
2015-12-11 23:54:16 +0100

    Added customization scripts
    Those scripts add the postgres Driver to wildfly on docker build and
    changes the run behaviour. Each starts create a suiteable JDBC
    Connection

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM jboss/wildfly
+
+RUN curl -o /tmp/psql-jdbc.jar https://jdbc.postgresql.org/download/postgresql-9.4-1201.jdbc41.jar
+
+ENV JBOSS_HOME /opt/jboss/wildfly
+
+RUN /opt/jboss/wildfly/bin/add-user.sh admin admin --silent
+
+ADD customization /opt/jboss/wildfly/customization
+
+RUN ["sh", "/opt/jboss/wildfly/customization/config.sh"]
+
+RUN rm -rf /opt/jboss/wildfly/standalone/configuration/standalone_xml_history/current
+
+EXPOSE 8080 8080
+
+RUN ["/opt/jboss/wildfly/customization/execute.sh"]
---
+++ customization/execute.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+JBOSS_HOME=/opt/jboss/wildfly
+JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
+JBOSS_MODE=${1:-"standalone"}
+JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
+
+function wait_for_server() {
+  until `$JBOSS_CLI -c ":read-attribute(name=server-state)" 2> /dev/null | grep -q running`; do
+    sleep 5
+  done
+}
+
+echo "=> Starting WildFly server"
+$JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -c $JBOSS_CONFIG &
+
+echo "=> Waiting for the server to boot"
+wait_for_server
+
+echo "=> Executing the commands"
+echo "=> Postgres (docker host): " $POSTGRES_PORT_5432_TCP_ADDR
+echo "=> Postgres (docker port): " $POSTGRES_PORT_5432_TCP_PORT
+
+$JBOSS_CLI -c << EOF
+
+xa-data-source add --name=PostgresXADS --driver-name=postgresql --jndi-name="java:jboss/datasources/postgres" --enabled=true --use-ccm=true --user-name=postgres --xa-datasource-class=org.postgresql.xa.PGXADataSource --xa-datasource-properties=[{url=jdbc:postgresql://$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT/bda_benchmarking}]
+
+EOF
+
+echo "=> Shutting down WildFly"
+if [ "$JBOSS_MODE" = "standalone" ]; then
+  $JBOSS_CLI -c ":shutdown"
+else
+  $JBOSS_CLI -c "/host=*:shutdown"
+fi
+
+echo "=> Restarting WildFly"
+$JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -c $JBOSS_CONFIG
+
